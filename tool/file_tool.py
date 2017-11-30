20656a1094287b9878b15f831a988ec1a110e3c8
Shaozhe <shaozhefeng@zju.edu.cn>
2017-11-30 13:35:00 +0800

    test new username

--- tool/file_tool.py
+++ tool/file_tool.py
@@ -3,22 +3,22 @@ import cPickle as pickle
 
 
 def load_gzip_pickle(file_path):
-    with gzip.open(file_path + '.pkl.gz', 'r') as f:
+    with gzip.open(file_path + '.pkl.gz', 'rb') as f:
         data = pickle.load(f)
         return data
 
 
 def write_gzip_pickle_file(file_path, data):
-    with gzip.open(file_path + '.pkl.gz', 'w') as f:
+    with gzip.open(file_path + '.pkl.gz', 'wb') as f:
         pickle.dump(data, f)
 
 
 def load_pickle(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'rb') as f:
         data = pickle.load(f)
         return data
 
 
 def write_pickle(file_path, data):
-    with open(file_path + '.pkl', 'w')as f:
+    with open(file_path + '.pkl', 'wb')as f:
         pickle.dump(data, f)
