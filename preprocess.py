dd9d57451e34b2fb3be1583aefd2df9c7d9b9d33
Shaozhe <shaozhefeng@zju.edu.cn>
2017-11-30 22:36:53 +0800

    test new username

--- preprocess.py
+++ preprocess.py
@@ -7,21 +7,14 @@ sep = os.path.sep
 
 
 def get_split_video(video_dir):
-    cor_list = [(639, 150, 798, 346),
-                (370, 55, 503, 195),
-                (820, 324, 962, 500),
-                (653, 165, 768, 330),
-                (364, 33, 501, 195),
-                (531, 16, 668, 204),
-                (679, 27, 833, 222),
-                (700, 29, 829, 188),
-                (532, 8, 668, 155),
-                (682, 91, 833, 261)
-                ]
+    cor_list = []
+    for video_name in os.listdir(video_dir):
+        video_path = os.path.join(video_dir, video_name)
+        coordinate, _ = get_coordinate(video_path)
+        cor_list.append(coordinate)
+
     for video_name, coordinate in zip(os.listdir(video_dir), cor_list):
         video_path = os.path.join(video_dir, video_name)
-
-        # coordinate, _ = get_coordinate(video_path)
 
         split_video(video_path, coordinate, '.' + sep + 'data' + sep + 'split_videos')
 
@@ -29,7 +22,7 @@ def get_split_video(video_dir):
 if __name__ == '__main__':
     # get_split_video('.' + sep + 'data' + sep + 'videos')
 
-    crop_dir =  '.' + sep + 'data' + sep + 'split_videos'
+    crop_dir = '.' + sep + 'data' + sep + 'split_videos'
     for crop_file in os.listdir(crop_dir):
         file_extension = os.path.splitext(crop_file)[1]
         file_name = os.path.splitext(crop_file)[0]
@@ -38,6 +31,6 @@ if __name__ == '__main__':
             frame = file_tool.load_pickle(crop_path)
             annotator = Annotator(file_name)
             frame_label = annotator.label_video(frame)
-    
-            label_name = 'label' + os.path.splitext(crop_file)[0]
+
+            label_name = 'label_' + os.path.splitext(crop_file)[0]
             file_tool.write_pickle(label_name, frame_label)
